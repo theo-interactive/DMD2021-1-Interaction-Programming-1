962cdb91626dc5056e41a6c41c3111ed1f04953b
Kim Yong Won <rh@102labs.com>
2021-04-15 01:18:04 +0900

    파일 변경

--- 6Week/#lecture/class/BOM/js/script.js
+++ 6Week/#lecture/class/BOM/js/script.js
@@ -2,197 +2,53 @@
 BOM
 브라우저의 객체 속성.
 Browser Object Modal
-
-브라우저가 가지고 있는 최상위 객체.(브라우저가 실행되면 생성되는 내장 객체)
-
-window - 브라우저 화면에 대한 객체.
-navigator - 데스크탑, 모바일의 환경에 대한 객체
-screen - 해상도 등의 모니터 화면에 대한 객체.
-hisotry - 문서의 브라우저 방문 기록에 대한 객체.
-location - 문서의 경로에 대한 객체. (http://~~~)
-
-*/
-// console.log(window);
-// console.log(document); // DOM - Document Object Model
-// console.log(window.innerWidth, window.innerHeight); // 브라우저의 화면 사이즈.(가로너비, 세로높이 px)
-/*
-window.addEventListener("resize", function() {
-    // 리사이즈 이벤트.
-    //console.log("resize");
-    console.log(window.innerWidth, window.innerHeight);
-});
-*/
-// console.log(window.screenTop, window.screenLeft); // 모니터 화면에서 브라우저의 위치.
-// console.log(window.pageXOffset, window.pageYOffset); // 문서 내부의 스크롤 위치.(문서가 길어져서 스크롤이 가능한 경우 확인)
-
-// console.log(window.location);
-// console.log(location);
-// URL 의 객체를 반환.
-
-// window.alert("경고창입니다."); // 시스템 경고창이 출력.
-// alert("경고창 2");
-
-// window.prompt("이름을 입력해주세요.");
-// var name = prompt("이름을 입력해주세요."); // 시스템 다이얼로그 창을 출력
-// (메시지 + 입력가능한 인풋, 확인 / 취소)
-// 취소를 누르게 되면 null 값이 반환.
-// 확인을 누르게 되면 입력한 문자열 값이 반환.
-// console.log(name);
-/*
-//window.confirm("확인되었습니다.");
-var isComplete = confirm("입력을 완료하셨나요?"); // 메시지 + 확인 / 취소 다이얼로그 창을 출력 
-// true / false 의 값을 반환.
-console.log(isComplete);
-if(isComplete){ // 확인인 경우. true
-    alert("확인되었습니다.");
-}
-if(!isComplete){ // 취소인 경우. false
-    alert("다시 입력해주세요.");
-}
-*/
-// window 객체의 메서드 중 하나. 문서를 해당 좌표로 스크롤 이동.
-// window.scrollTo(x, y); //숫자로 입력.
-// window.scrollTo(0, 0); // 문서의 최상단으로 이동.
-// window.scrollTo(0, 500); // x: 0px, y: 500px 의 좌표로 이동
-
-/*
-window.setInterval();
-window.clearInterval();
-
-window.setTimeout();
-window.clearTimeout();
-*/
-// setInterval();
-// 주기적으로 반복되는 타이머 생성.
-// setInterval(function() {
-//     console.log("interval-1");
-// }, 1000); // 1초마다 코드 실행.
-// setInterval(callback(handler), time); milliseconds - 1000/1 초(ms)
-/*
-var i = 0;
-function loop() {
-    console.log("interval-2");
-    // clearInterval(timer); // 타이머가 발생되면 초기화.
-    if(i === 10){
-        clearInterval(timer); // 타이머가 발생되면 초기화.
-    }
-    i++;
-}
-var time = 500;
-*/
-// var timer = setInterval(loop, time); // 1초마다 loop 함수 호출
-// 타이머를 제거.
-// clearInterval(timer); // timer 변수의 Interval 을 초기화.(제거)
-
-// setTimeout(callback(handler), time); // 한번만 실행되는 타이머.
-// clearTimeout(timer);
-// clearInterval(timer); // setTimeout 으로 실행한 타이머도 초기화 가능.
-// var timer2 = setTimeout(loop, time);
-/*
-var _timer;
-i = 0;
-function setLoop(){
-    console.log("loop");
-    if(i < 10){
-        setTime();
-    }
-    i++;
-}
-function setTime() {
-    _timer = setTimeout(setLoop, 1000);
-}
-setTime();
 */
 
-// 새로운 내용의 브라우저 창 열기.
-//window.open("http://www.naver.com", "naver");
-// window.open(URL 주소, 이름, 스펙, 방문이력을 대체할지?(true, false));
-//window.open("http://google.com", "_blank", "top=500,left=500,width=1000,height=500");
+// Navigator 객체.
+// 브라우저의 정보
+// console.log(window.navigator);
+// console.log(navigator);
+// console.log(navigator.appName);
+// console.log(navigator.appVersion);
+// console.log(navigator.userAgent);
+// 사용자가 어떤 플랫폼, OS, 브라우저 환경 에서 접속했는지 파악할 수 있다.
+// PC, Mobile 중 어디로 접속했는지 체크하거나 할 수 있다.
 
-// Specs.
-// top - 브라우저의 스크린  Y 좌표(px)
-// left - 브라우저의 스크린 X 좌표(px)
-// width - 브라우저의 가로폭 너비(px)
-// height - 브라우저의 세로 높이(px)
-// titlebar - 브라우저가 가지고 있는 제목 표시줄의 여부 yes no 1 0
-// menubar - 메뉴 표시의 여부
-// scrollbar - 스크롤바가 노출될 지 여부
-// toolbar - 도구모음 표시의 여부
-// location - 주소 표시줄의 여부
-// status - 상태 표시줄의 여부
-// resizable - 리사이즈의 가능 여부
-// fullscreen - 풀스크린의 여부
+// Screen 객체
+// 화면의 정보
+// console.log(window.screen);
+// console.log(screen);
+// console.log(screen.width);
+// console.log(screen.height);
+// console.log(screen.availWidth);
+// console.log(screen.availHeight);
+// console.log(screen.orientation.angle); // 화면의 각도.
 
-// window.close();
-// 브라우저 창을 닫기.
+// 모바일의 각도.
+// 90 angle - Landscape view (풍경화)
+// 0 angle - Portrait view (인물화)
 
-// 브라우저, 데스크탑, 모바일의 정보 객체
-/*
-console.log(navigator);
-console.log(navigator.appName);
-console.log(navigator.appVersion);
-console.log(navigator.userAgent); // 어떤 디바이스로 접속했는지 체크할 때 사용.
-console.log(navigator.userAgent.match("Mac").length > 0); // Mac 을 사용하고 있군..
-console.log(navigator.userAgent.match("Windows") !== null) // Windows 를 사용하고 있군...
-*/
-// if(navigator.userAgent.match("Mac"))
-// 모니터 화면에 대한 정보.
-console.log(window.screen);
-console.log(screen);
-console.log(screen.availTop, screen.availLeft, screen.availWidth, screen.availHeight);
-console.log(screen.width, screen.height);
-
-
-// history.
-// 사용자가 방문한 방문 이력 정보를 담고 있는 객체.
+// History 객체
+// (브라우저를 새로 열고 난 후) 방문이력 정보
 // console.log(window.history);
 // console.log(history);
-/* 방문이력이 이동 되었다면 1 초과(2 이상) */
-// console.log(history.length); // 방문 이력에 대한 개수. 최초 문서를 로드할 때, 1 반환.
+// console.log(history.length); // 현재까지 방문한 이력의 개수.
+// history.back(); // 뒤로 가기 기능
+// history.forward(); // 앞으로 가기 기능
+// history.go(-1); // 현재 보고있는 페이지를 기준으로 이동.
+// history.go(1);
 
-//** 모바일 웹을 만들 때, 뒤로 가기 버튼을 넣어야 하는지, 빼야하는지 확인할 때.
-// if(history.length > 1){
-    // 뒤로 가기 버튼 추가.
-// }else{
-    // 뒤로 가기 버튼 삭제.
-// }
-
-//**
-// history.back();
-// 이전 방문 이력으로 이동.
-
-// history.forward();
-// 다음 방문 이력으로 이동. (다음 방문이력이 있을 때에만 동작)
-
-// history.go(-1); 방문 이력을 기준으로 뒤로 가기.
-// index 번호에 따라 이동.
-
-// location.
-// URL 정보 객체.
+// Location 객체
+// URL 주소 정보
 // console.log(window.location);
 // console.log(location);
-
-// **
-// console.log(location.href);
-// URL 경로 전체를 문자열로 반환.(get)
-// location.href = "http://google.com"; // 해당 URL 경로로 이동.
-
-// *
-// location.hash;
-// hash 태그를 이용해서 스크롤 이동하거나 할 수 있다.
-// spa - single page application (원페이지 사이트)
-// 각 섹션별로 about, work, contact.
-// URL/#about, URL/#work, URL/#contact
-// 주소가 있는 원페이지 사이트를 만들 때, 주로 사용.
-
-// *
-// location.reload();
-// 문서 새로고침.
-
-// location.host - 호스트네임(포트포함)을 반환(도메인)
-// location.hostname - 호스트네임을 반환
-// location.port - 포트를 반환
-// location.search - URL 매개변수를 반환.
-// location.protocol - 프로토콜(http:, https:, file:, ftp:, sftp:, svn:....)을 반환
-// location.pathname - 현재 보고있는 페이지의 경로 / - index 페이지, /about - about 페이지
-
+// search
+// protocol
+// origin
+// hash
+// pathname
+// ----
+// location.reload(); 새로고침
+// location.replace("http://naver.com"); 주소 변경(대체) - 현재 보고있던 마지막 방문이력이 바뀜.
+// location.assign("http://naver.com"); 주소 변경 - 방문이력이 추가됨.
+// location.href = "http://google.com";
--- 6Week/#lecture/class/DOM/js/script.js
+++ 6Week/#lecture/class/DOM/js/script.js
@@ -1,236 +1,114 @@
-console.log(window.document);
-console.log(document);
-// 문서.
 /*
 DOM
-
-Document Object Model.
-문서(html) 객체화
+문서의 객체 속성.
+Document Object Modal
 */
-console.log(document.documentElement);
-// 문서 객체를 노드화 해서 반환.
-// html 태그 요소.
 
-console.log(document.body);
-// body 태그 요소.
+// console.log(window.document);
+// console.log(document);
 
-console.log(document.scripts);
-console.log(document.scripts.length);
-// script 태그 요소들을 반환.(목록-배열)
+// console.log(document.documentElement);
+// <html> 노드 반환
+// console.log(document.body);
+// <body> 노드 반환
 
-console.log(document.title);
-// title 태그의 값을 반환. // get
+// cookie
+// domain
+// console.log(document.scripts);
 
-document.title = "제목 변경";
-// title 태그의 값이 변경. // set
+// console.log(document.title); // 문서의 제목 (get)
+// document.title = "제목" // 문서의 제목 변경 (set)
 
-var divEl = document.createElement("div");
-console.log(divEl);
-// <div></div>
-// JavaScript 공간에 새로운 Div 요소를 생성.
-var spanEl = document.createElement("span");
-console.log(spanEl);
-// <span></span>
-// element.innerText = "String"; 
-// 요소 안에 텍스트 기입.
-// divEl.innerText = "안녕하세요.";
-// spanEl.innerText = "반갑습니다.";
+// createElement();
+var div = document.createElement('div');
+// console.log(div);
+// 매개변수에 명칭되어있는 해당 태그로 요소(노드) 생성. <div></div>
 
-// 요소 안에 HTML 형식의 값을 기입.
-// divEl.innerHTML = "<span>안녕하세요.</span>";
-// console.log(divEl);
-// console.log(spanEl);
-// <div>안녕하세요.</div>
-// <span>반갑습니다.</span>
+var header = document.createElement("header"); // <header></header>
+// console.log(header);
 
-var boxDivEl = document.getElementById("box-div");
-console.log(boxDivEl);
-// 전달인자의 값과 동일한 id 이름의 요소를 반환. 1개만 반환.
-// 전달인자의 값과 동일한 id 이름의 요소가 없을 경우 null 반환.
+var a = document.createElement("a"); // <header></header>
+// console.log(a);
 
-var boxEls = document.getElementsByClassName("box");
-// 전달인자의 값과 동일한 class 이름의 요소들을 반환.
-// 복수의 목록으로 반환 [element, element,...., element] 배열.
+// script 상에서 태그(노드) 생성.
 
-console.log(boxEls);
-console.log(boxEls[0]);
-// Elements 를 찾게되면 배열로 반환되기 때문에 원소 순서에 따라 요소를 검색할 수 있다.
-console.log(boxEls[boxEls.length - 1]); // 마지막 요소 반환.
+// id 로 된 요소 찾기.
+var titleEl = document.getElementById('title');
+// console.log(titleEl);
 
-// 요소 노드를 찾아오는 방법.
-var boxDivEl = document.getElementById("box-div");
-var boxEls = document.getElementsByClassName("box");
-var userIdEl = document.getElementsByName("user_id")[0];
-var divEls = document.getElementsByTagName("div");
-var ulEl = document.getElementsByTagName("ul")[0];
+// class 이름으로 된 요소들 찾기.
+var boxEls = document.getElementsByClassName('box');
+// console.log(boxEls); // Node 목록으로 반환.
 
-// **
-for(var i = 0; i < boxEls.length; i++) {
-    var boxEl = boxEls[i];
-    boxEl.innerText = "new box " + parseInt(i + 1, 10);
-    boxEl.style.backgroundColor = "#000000";
-    boxEl.style.color = "#ffffff";
-    // console.log(boxEls[i]);
+// name 속성 이름으로 된 요소들 찾기.
+var inputEls = document.getElementsByName('user_id');
+// console.log(inputEls);
+
+// tag 이름으로 된 요소들 찾기.
+var divEls =document.getElementsByTagName('div');
+// console.log(divEls);
+
+// write('string');
+document.write("hello~~~~~");
+// 문서의 맨 하단 (body 바로 위)에 내용 추가.
+
+// DOM Element
+// console.log(titleEl.attributes);
+// 해당하는 요소가 가지고 있는 속성들
+// console.log(inputEls[0].attributes);
+
+var ulEl = document.getElementById('list');
+// console.log(ulEl.childNodes); // Node(모든 자식 항목 - 공백 텍스트 포함)
+// console.log(ulEl.children); // Element(모든 자식 요소)
+// console.log(ulEl.children[1]);
+
+for (var i = 0; i < 3; i++) {
+    // console.log(i, ulEl.children[i]);
 }
 
-// attributes
-// 요소의 할당된 속성을 객체로 반환.
-// console.log(userIdEl.attributes);
+var li2El = ulEl.children[1];
+// console.log(li2El.classList); // 해당 요소가 가지고 있는 클래스 이름들.
+var li2Class = li2El.classList;
 
-// query
-/*
-#id{}
-.class{}
-[name=user_id]{}
-div{}
-ul{}
-*/
 
-// document.querySelector();
-// 1개의 요소 노드만 반환.
-var h1TitleEl = document.querySelector("h1#title.title");
-// console.log(h1TitleEl);
-// element.querySelectorAll();
-// 2개 이상의 요소 노드목록 반환.
-var listEls = ulEl.querySelectorAll("li.item");
-// console.log(listEls);
-// 해당 요소의 바로 아래 항렬에 위치한 자식요소들을 노드 목록 반환.
-// children
-listEls = ulEl.children;// (요소들만 반환)
-// console.log(listEls);
-// childNodes
-// var listNodes = ulEl.childNodes; // (공백이 있다면 공백도 Node 로 인식)
-// console.log(listNodes);
-
-// classList
-// 요소 노드의 클래스 속성값 목록을 반환.
-// console.log(listEls[1].classList);
-// console.log(listEls[2].classList.contains("size")); // false
-// console.log(listEls[2].classList.contains("item")); // true
-
-// classList.add("클래스명"); // 클래스 명 추가.
-// classList.remove("클래스명"); // 클래스 명 삭제.
-var li2Cl = listEls[2].classList;
-if(!li2Cl.contains("size")) {
-    // li2Cl.add("size");
-}
-if(listEls[2].classList.contains("item")) {
-    // listEls[2].classList.remove("item");
-}
-
-// classList.toggle(); // 클래스 명을 추가하거나 삭제하거나.
-// 코드가 처음 실행될 때, add() 의 기능.
-// 코드가 두 번째 실행될 때, remove() 의 기능.
-// .... 세 ...., add();
-// .... 네 ...., remove();
-
-// listEls[0].classList.toggle("red"); // add() | remove();
-// listEls[0].classList.toggle("red", true); // add();
-// listEls[0].classList.toggle("red", false); // remove();
-
-// console.log(h1TitleEl.id); // 요소 노드의 id 값을 반환.
-// console.log(h1TitleEl.className); // 요소 노드의 class 값을 반환.
-
-// firstChild
-// firstElementChild
-// lastChild
-// lastElementChild
-console.log(ulEl.firstChild); // 노드가 포함. (첫번째 노드 항목) 요소간의 사이에 공백이 있는 경우, Text " "
-console.log(ulEl.firstElementChild); // 요소 노드만 반환. (첫번째 요소 노드 항목)
-console.log(ulEl.lastChild); // 마지막 노드 항목(자식 노드)
-console.log(ulEl.lastElementChild); // 마지막 요소 노드(자식 요소 노드)
-
-console.log(listEls[listEls.length - 1].innerText); // get.
-console.log(listEls[listEls.length - 1].innerHTML);
-console.log(listEls[listEls.length - 1].style); // 요소의 스타일을 반환.
-console.log(listEls[listEls.length - 1].style.color);
-console.log(listEls[listEls.length - 1].style.backgroundColor);
-/*
-background-color : CSS 표기법
-backgroundColor : JavaScript 표기법
-*/
-listEls[listEls.length - 1].innerText = "new item 3"; // set.
-listEls[listEls.length - 1].innerHTML = "<span><b>new item 3</b></span>";
-listEls[listEls.length - 1].style.width = 800 + "px";
-listEls[listEls.length - 1].style.backgroundColor = "black";
-listEls[listEls.length - 1].style.color = "white";
-
-// addEventListener();
-// removeEventListener();
-/*
-DOM Event.
-마우스, 키보드, 센서... 등의 사용자 조작을 했을 때 발생.
-*/
-// 요소 노드에 이벤트를 추가 / 제거.
-var aEl = document.querySelector("a.link");
-/*
-aEl.addEventListener("click", function(event) {
-    // console.log(event); // 클릭된 요소, 이벤트의 정보 객체.
-    event.preventDefault();
-    // 태그 요소가 가지고 있는 기본 이벤트를 발생하지 않도록 방지.
-    console.log("a tag click");
-});
-*/
-// var onClickA = function(e) {
+// 요소에 클래스명 추가.
+// classList.add();
+li2El.classList.add('red');
+li2Class.add('red', 'padding');
+// 요소에 클래스명 삭제.
+// classList.remove();
+li2El.classList.remove('size');
+li2Class.remove('size', 'red');
+// classList.toggle();
+// 요소에 클래스명 추가 또는 삭제
+// (클래스가 없을 추가 / 클래스 있을 때 삭제)
+li2Class.toggle('size');
+li2Class.toggle('red', false); // remove('red');
+li2Class.toggle('size', true); // add('size');
+// 2번째 매개변수가
+// true 일 때 add()
+// false 일 때 remove()
+// classList.contains();
+// if (true) {
 // }
-// aEl.addEventListener("click", onClickA(evt)); - X
-aEl.addEventListener("click", onClickA); // - O
-// aEl.removeEventListener("click", onClickA); // 요소 노드에 할당된 이벤트 핸들러를 제거.
-// aEl.addEventListener("dblclick", onClickA);
 
-// 이벤트 핸들러 함수를 정의.
-
-var id = 1;
-function onClickA(event) {
-    event.preventDefault();
-    console.log("a tag click");
-    // ulEl.style.fontSize = 40 + "px";
-    // ulEl.style.lineHeight = 60 + "px";
-    // ulEl.style.backgroundColor = "#000000";
-    // ulEl.style.color = "#ffffff";
-
-    // var newItemEl = document.createElement("li");
-    // console.log(newItemEl); // <li></li>
-    // newItemEl.classList.add("item");
-    // newItemEl.innerText = "new item " + id;
-    // newItemEl.style.padding = 50 + "px";
-    // ulEl.appendChild(newItemEl); // ul 의 마지막 부분에 li 요소 추가.
-    // // ulEl.removeChild(listEls[0]); // 해당 요소를 ul 에서 삭제.
-    // ulEl.removeChild(listEls[1]);
-    // id++;
-
-    var url = aEl.getAttribute("href");
-    // window.open(url, "새로운 윈도우");
-    // location.href = url;
-    var placeholder = userIdEl.getAttribute("placeholder");
-    // console.log(placeholder);
-    // userIdEl.setAttribute("placeholder", "새로운 아이디를 입력해주세요.");
-    // console.log(userIdEl.hasAttributes()); // true
-
-    var articleEl = document.getElementsByTagName("article")[0];
-    // console.log(articleEl.hasAttributes()); // false
-
-    // userIdEl.removeAttribute("placeholder");
-    // placeholder="" 이렇게 남아 있는 상태가 아님.
-    // 속성 항목 자체를 삭제해버림.
-
-    // userIdEl.setAttribute("value", "아이디"); // 문서 상에서 변경.
-    // userIdEl.value = "value 아이디 지정"; // 요소 상에서 현재의 값으로 변경. **
-
-    var cloneItemEl = listEls[0].cloneNode(); // 노드 복제.
-    // console.log(cloneItemEl);
-    // cloneItemEl.innerText = "new item";
-
-    // ulEl.insertBefore(cloneItemEl, listEls[listEls.length - 1]);
-    // el.insertBefore(el1, el2);
-    // el 의 내부(자식 노드)에 el1(새로운 요소)가 el2의 앞쪽에 위치되어 추가.
+// 해당 요소에 클래스가 있는지 없는지 확인. boolean 반환 (true/false)
+if (li2Class.contains('padding')) { // true
+    // console.log('padding');
 }
-// appendChild();
-// removeChild();
+if (!li2Class.contains('size')) { // false
+    // console.log('size');
+}
 
-// getAttribute("속성");
-// hasAttributes();
-// removeAttribute();
-// setAttribute();
+// 속성에 대한 값을 반환.
+// console.log(inputEls[0].id); // id 속성
+// console.log(inputEls[0].className); // class 속성
 
-// insertBefore();
+console.log(ulEl.firstChild); // Node 를 포함한 첫번째 자식 항목.
+console.log(ulEl.firstElementChild); // 첫번째 자식 요소.
+console.log(ulEl.lastChild); // 마지막 자식 항목.
+console.log(ulEl.lastElementChild); // 마지막 자식 요소.
+
+var boxDivEl = document.getElementById('box-div');
+console.log(boxDivEl.innerHTML); // get
+boxDivEl.innerHTML = '<h2>안녕하세요.</h2>'; // set
